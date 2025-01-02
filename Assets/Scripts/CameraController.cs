40cf0cb5d80b61fa62c9a71c203e46fdd7bf8cb6
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-01-02 14:41:12 +0900

    [add] 三人称視点カメラ

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,8 +9,16 @@ public class CameraController : MonoBehaviour
     
     [SerializeField, Header("オフセット")] private Vector3 offset;
 
-    void Update()
+    [SerializeField, Header("追従する速度")] private float smoothSpeed = 0.125f;
+    
+    private Vector3 _currentOffset;
+
+    void LateUpdate()
     {
-        
+        _currentOffset = player.transform.rotation * offset;
+        var desiredPos = player.transform.position + _currentOffset;
+        var fixedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = fixedPos;
+        transform.LookAt(player.transform.position + Vector3.up * 0.75f);
     }
 }
