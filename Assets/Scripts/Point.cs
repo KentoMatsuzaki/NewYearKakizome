b43f51860e0c938116fd26975f9088346a3aae89
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-01-05 23:12:45 +0900

    [add] プレイヤーの角度を変更する処理を追加

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class Point : MonoBehaviour
 {
+    [SerializeField, Header("プレイヤー")] private Transform player;
+
+    [SerializeField, Header("回転角度")] private float rotationY;
     
+    [SerializeField, Header("トレイル")] private Transform trail;
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        player.rotation = Quaternion.Euler(player.rotation.x, rotationY, player.rotation.z);
+        trail.gameObject.SetActive(true);
     }
 }
